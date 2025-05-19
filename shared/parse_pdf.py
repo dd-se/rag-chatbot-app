57ade590d15514289cb08312487388ee32bba2e5
dd-se <76243082+dd-se@users.noreply.github.com>
2025-05-19 12:19:29 +0200

    Add logging

--- shared/parse_pdf.py
+++ shared/parse_pdf.py
@@ -1,16 +1,26 @@
+import io
+
 from pypdf import PdfReader
 
+from .logging_helper import get_logger
 
-def load_pdf_data(content):
+logger = get_logger(__name__)
+
+
+def load_pdf_data(content: io.BufferedReader):
+    logger.debug(f"{content.name = }")
     reader = PdfReader(content)
     text = ""
     for page in reader.pages:
         text += page.extract_text()
+    logger.info("Text extracted successfully.")
     return text
 
 
-def chunk_text(text, chunk_size=1000, overlap=200):
+def chunk_text(text: str, chunk_size: int = 1000, overlap: int = 200) -> list[str]:
     chunks = []
     for i in range(0, len(text), chunk_size - overlap):
         chunks.append(text[i : i + chunk_size])
+    logger.debug(f"{len(text) =  } | {chunks = } | {chunk_size = } | {overlap = }")
+    logger.info("Text splitted into chunks successfully.")
     return chunks
