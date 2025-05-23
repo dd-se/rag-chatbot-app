d7dc985dd69d0a0ac2bc528a405b50000a76f8f0
dd-se <76243082+dd-se@users.noreply.github.com>
2025-05-23 11:52:36 +0200

    Improve PDF text cleaning

--- shared/parse_pdf.py
+++ shared/parse_pdf.py
@@ -1,4 +1,5 @@
 import io
+import re
 from pathlib import Path
 
 from pypdf import PdfReader
@@ -8,17 +9,34 @@ from .logging_helper import get_logger
 logger = get_logger(__name__)
 
 
-def load_pdf_data(content: io.BufferedReader | Path):
-    logger.debug(f"{content.name = }")
+def load_pdf_data(content: io.BufferedReader | Path) -> str:
+    """Extracts and cleans text from a PDF file."""
     reader = PdfReader(content)
-    text = ""
-    for page in reader.pages:
-        text += page.extract_text()
-    logger.info("Text extracted successfully.")
-    return text
+    cleaned_text_parts = []
+    for i, page in enumerate(reader.pages, 1):
+        try:
+            raw_text = page.extract_text()
+            # Skip pages with little text
+            if not raw_text or len(raw_text.strip()) < 50:
+                continue
+        except Exception:
+            logger.error(f"Error extracting text from page {i}: {Exception}")
+            continue
+        # Remove non-ASCII characters
+        text = raw_text.encode("ascii", "ignore").decode()
+        # Remove line break hyphenation (co-\noperation → cooperation)
+        text = re.sub(r"-\s+", "", text)
+        # Remove whitespace (newlines, multiple spaces)
+        text = re.sub(r"\s+", " ", text)
+        # Remove spacing around punctuation (word ! → word!)
+        text = re.sub(r'\s([?.!"](?:\s|$))', r"\1", text)
+        text = text.lower()
+        cleaned_text_parts.append(text.strip())
+    logger.info("Text extracted from the document successfully.")
+    return " ".join(cleaned_text_parts)
 
 
-def chunk_text(text: str, chunk_size: int = 1000, overlap: int = 200) -> list[str]:
+def chunk_text(text: str, chunk_size: int = 1024, overlap: int = 200) -> list[str]:
     chunks = []
     for i in range(0, len(text), chunk_size - overlap):
         chunks.append(text[i : i + chunk_size])
