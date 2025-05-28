a7433b3cd4d58b86fa7542d06acd2c9155a3bfb2
dd-se <76243082+dd-se@users.noreply.github.com>
2025-05-28 13:46:53 +0200

    Refactor DeepEval RAG tests

--- tests/test_rag.py
+++ tests/test_rag.py
@@ -1,3 +1,4 @@
+# Add example RAG tests for demonstration purposes
 # Adapted from https://github.com/meteatamel/genai-beyond-basics/blob/main/samples/evaluation/deepeval/rag_eval/test_rag_triad_cymbal.py
 # > deepeval test run tests/test_rag.py
 import os
@@ -11,27 +12,28 @@ from deepeval.test_case import LLMTestCase
 from shared import *
 
 logger = get_logger(__name__)
+qa_file = "example/test.json"
+pdf_document = "example/test.pdf"
 eval_model = GeminiModel(model_name="gemini-2.0-flash-lite", api_key=os.environ["GEMINI_API_KEY"])
 answer_relevancy = AnswerRelevancyMetric(model=eval_model)
 faithfulness = FaithfulnessMetric(model=eval_model)
 
-with open("example/test.json", encoding="utf-8") as f:
+with open(qa_file, encoding="utf-8") as f:
     qa_list = qa_list_adapter.validate_json(f.read())
-with open("example/test.pdf", "rb") as f:
+with open(pdf_document, "rb") as f:
     doc_hash = get_document_hash(f)
+    assert is_in_db(doc_hash) is True, f"Please add the document {pdf_document} to the vector store before proceeding with the tests."
 
 
 @pytest.mark.parametrize("qa", qa_list)
 def test_rag(qa: QAItem):
     embedding = create_embeddings([qa.question])[0].values
-    top_chunks = get_relevant_context(embedding, doc_hash)["documents"][0]
+    top_chunks = get_relevant_context(embedding, doc_hash)
     response = context_aware_response(qa.question, top_chunks).text
-
     test_case = LLMTestCase(
         input=qa.question,
         actual_output=response,
         expected_output=qa.ideal_answer,
         retrieval_context=top_chunks,
     )
-    logger.info(f"Evaluating: {qa.question[:20]}")
     assert_test(test_case, [answer_relevancy, faithfulness], run_async=False)
